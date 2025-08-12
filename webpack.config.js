f0c840f548b72464f3a989e9eee56ffc22c6bc7b
Vincent Nguyen <vincent.nguyen@hugeinc.com>
2025-08-12 17:46:10 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'public'),
     },
     module: {
@@ -26,8 +28,19 @@ module.exports = {
         ],
     },
     plugins: [
+        new CleanWebpackPlugin({
+            // THIS IS THE MODIFIED PART
+            // Specify exact patterns for files to remove.
+            cleanOnceBeforeBuildPatterns: [
+                'main.*.js',   // Matches main.a1b2c3.js, main.d4e5f6.js, etc.
+                'index.html',  // Matches the single index.html file
+            ],
+        }),
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
     ],
+    devServer: {
+        static: './public',
+    },
 };
